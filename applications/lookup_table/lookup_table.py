89afaa54a5941e394a42bea0cd8e2171f9a2171c
VodeniZeko <salexy.es@gmail.com>
2020-08-06 14:17:32 -0700

    lookup

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,4 +1,7 @@
 # Your code here
+import math
+import random
+table = {}
 
 
 def slowfun_too_slow(x, y):
@@ -9,17 +12,24 @@ def slowfun_too_slow(x, y):
 
     return v
 
+
 def slowfun(x, y):
     """
     Rewrite slowfun_too_slow() in here so that the program produces the same
     output, but completes quickly instead of taking ages to run.
     """
     # Your code here
-
+    key = f"x{x}y{y}"
+    if key not in table:
+        v = math.pow(x, y)
+        v = math.factorial(v)
+        v //= (x + y)
+        v %= 982451653
+        table[key] = v
+    return table[key]
 
 
 # Do not modify below this line!
-
 for i in range(50000):
     x = random.randrange(2, 14)
     y = random.randrange(3, 6)
